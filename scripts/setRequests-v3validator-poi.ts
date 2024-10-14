0033ef6be661ce1d63f0a5ed13576e5379fe4370
Amin Latifi <a.latifi.al@gmail.com>
2024-10-14 14:06:55 +0330

    Updated contracts

--- scripts/setRequests-v3validator-poi.ts
+++ scripts/setRequests-v3validator-poi.ts
@@ -153,8 +153,8 @@ const poiLd = `{
   ]
 }`;
 async function main() {
-  const validatorAddressV3 = '0x48a16180b004726cc12bF55C61A2167Da16CA0f3';
-  const erc20verifierAddress = '0xB752Eec418f178ac8B48f15962B55c37F8D4748d';
+  const validatorAddressV3 = '0xd179f29d00Cd0E8978eb6eB847CaCF9E2A956336';
+  const erc20verifierAddress = '0xfcc86A79fCb057A8e55C6B853dff9479C3cf607c';
   const excludedCountryCodes = Object.values(KYC_EXCLUDED_COUNTRIES).sort((a, b) => a - b);
 
   const UniversalVerifierFactory = await ethers.getContractFactory('UniversalVerifier');
@@ -171,7 +171,7 @@ async function main() {
     method: DidMethod.Iden3
   });
 
-  const requestId = 10;
+  const requestId = 11;
   const countryNIN = {
     requestId,
     schema: schema,
@@ -217,7 +217,7 @@ async function main() {
       transaction_data: {
         contract_address: await universalVerifier.getAddress(),
         method_id: 'b68967e2',
-        chain_id: 2442,
+        chain_id: 1101,
         network: 'zkevm_cardona'
       },
       scope: [
